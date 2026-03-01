e4831b7aaca1fe2f68dd884176affd65c00a8dad
qwiful <pnv270984@gmail.com>
2026-03-02 00:21:32 +0300

    chore: add prettier, eslint and .gitkeep

---
+++ api/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: latest,
+    sourceType: 'module',
+  },
+  rules: {
+    'node/no-unpublished-require': 'off',
+  },
+}
