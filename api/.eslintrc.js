f9119052fb8875b275cbe68e85ea731844df69f9
qwiful <pnv270984@gmail.com>
2026-03-04 23:58:12 +0300

    chore: add scripts  for migrations, update eslint configuration

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -3,14 +3,9 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: latest,
-    sourceType: 'module',
+    ecmaVersion: 12,
   },
   rules: {
     'node/no-unpublished-require': 'off',
