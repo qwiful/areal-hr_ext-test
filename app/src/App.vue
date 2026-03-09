a47beaea4b97ce932b5b740bee8960874455c353
qwiful <pnv270984@gmail.com>
2026-03-09 22:18:37 +0300

    feat(app): setup project

---
+++ app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    <aside class="sidebar">
+      <nav>
+        <ul>
+          <li><router-link to="/">Профиль</router-link></li>
+          <li><router-link to="/organizations">Организации</router-link></li>
+          <li><router-link to="/departments">Отделы</router-link></li>
+          <li><router-link to="/positions">Должности</router-link></li>
+          <li><router-link to="/workers">Сотрудники</router-link></li>
+          <li><router-link to="/personnel-operations">Кадровые операции</router-link></li>
+          <li><router-link to="/history">История изменений</router-link></li>
+          <li><router-link to="/users">Пользователи</router-link></li>
+        </ul>
+      </nav>
+    </aside>
+
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+// тут потом добавить логику проверки авторизации и т.п.
+</script>
