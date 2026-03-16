57f93a612a270ced6cd0db2ce7cf6eb859b64943
qwiful <pnv270984@gmail.com>
2026-03-16 20:00:07 +0300

    feat(app/): update router, api client, global styles and vite config

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,14 +3,14 @@
     <aside class="sidebar">
       <nav>
         <ul>
-          <li><router-link to="/">Профиль</router-link></li>
           <li><router-link to="/organizations">Организации</router-link></li>
           <li><router-link to="/departments">Отделы</router-link></li>
           <li><router-link to="/positions">Должности</router-link></li>
           <li><router-link to="/workers">Сотрудники</router-link></li>
           <li><router-link to="/personnel-operations">Кадровые операции</router-link></li>
           <li><router-link to="/history">История изменений</router-link></li>
-          <li><router-link to="/users">Пользователи</router-link></li>
+          <li><router-link to="/specialists">Пользователи</router-link></li>
+          <li><router-link to="/">Выйти</router-link></li>
         </ul>
       </nav>
     </aside>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,9 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import OrganizationsView from '../views/OrganizationsView.vue'
 import DepartmentsView from '../views/DepartmentsView.vue'
 import PositionsView from '../views/PositionsView.vue'
-//import WorkersView from '../views/WorkersView.vue'
-//import PersonnelOperationsView from '../views/PersonnelOperationsView.vue'
-//import HistoryView from '../views/HistoryView.vue'
+import WorkersView from '../views/WorkersView.vue'
+import PersonnelOperationsView from '../views/PersonnelOperationsView.vue'
+import HistoryView from '../views/HistoryView.vue'
 //import SpecialistsView from '../views/SpecialistsView.vue'
 //import LoginView from '../views/LoginView.vue'
 
@@ -13,13 +13,13 @@ const routes = [
   { path: '/organizations', component: OrganizationsView, meta: { title: 'Организации' } },
   { path: '/departments', component: DepartmentsView, meta: { title: 'Отделы' } },
   { path: '/positions', component: PositionsView, meta: { title: 'Должности' } },
-  // { path: '/workers', component: WorkersView, meta: { title: 'Сотрудники' } },
-  // {
-  //   path: '/personnel-operations',
-  //   component: PersonnelOperationsView,
-  //   meta: { title: 'Кадровые операции' },
-  // },
-  // { path: '/history', component: HistoryView, meta: { title: 'История изменений' } },
+  { path: '/workers', component: WorkersView, meta: { title: 'Сотрудники' } },
+  {
+    path: '/personnel-operations',
+    component: PersonnelOperationsView,
+    meta: { title: 'Кадровые операции' },
+  },
+  { path: '/history', component: HistoryView, meta: { title: 'История изменений' } },
   // { path: '/specialists', component: SpecialistsView, meta: { title: 'Пользователи' } },
   // { path: '/login', component: LoginView, meta: { title: 'Вход' } },
 ]
