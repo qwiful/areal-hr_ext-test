8c5fbc02d6acd8d0e47a207bf737c384371bc4c4
qwiful <pnv270984@gmail.com>
2026-03-30 14:04:17 +0300

    fix(backend): change specialists routes

--- api/src/routes/specialists.js
+++ api/src/routes/specialists.js
@@ -1,14 +1,14 @@
 const { Router } = require('express')
 const router = Router()
-const pool = require('../db.js')
-const createSpecialistsController = require('../controllers/specialists.js')
+const pool = require('../db')
+const createSpecialistsController = require('../controllers/specialists')
 
 const specialistsController = createSpecialistsController(pool)
 
-router.post('/', specialistsController.createSpecialist)
 router.get('/', specialistsController.getSpecialists)
 router.get('/roles', specialistsController.getRoles)
 router.get('/:id', specialistsController.getSpecialistById)
+router.post('/', specialistsController.createSpecialist)
 router.put('/:id', specialistsController.updateSpecialist)
 router.delete('/:id', specialistsController.deleteSpecialist)
 
